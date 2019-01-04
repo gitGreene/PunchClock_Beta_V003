ab4416f03326dbf0d3f19314c4791df90d5789d0
Francis <francisglacken@gmail.com>
2019-01-04 08:49:16 -0500

    IT WORKS, LETS GO CHAMP!!! As in the detailRecyclerView is now properly showing different time values depending on the selected category

--- app/src/main/java/co/codemaestro/punchclock_beta_v003/CategoryDatabase.java
+++ app/src/main/java/co/codemaestro/punchclock_beta_v003/CategoryDatabase.java
@@ -8,7 +8,7 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 
-@Database(entities = {Category.class, TimeBank.class }, version = 12, exportSchema = false)
+@Database(entities = {Category.class, TimeBank.class }, version = 14, exportSchema = false)
 public abstract class CategoryDatabase extends RoomDatabase {
 
     // Dao
