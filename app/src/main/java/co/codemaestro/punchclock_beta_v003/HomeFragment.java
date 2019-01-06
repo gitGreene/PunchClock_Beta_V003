c33ba1cfb3a95d5f852a0e6e905a9ef516c1fb65
Mitch Greene <m.iangreene@gmail.com>
2019-01-06 00:44:06 -0500

    Day 094 of 100DoCC; Fragmented the top level views.

---
+++ app/src/main/java/co/codemaestro/punchclock_beta_v003/HomeFragment.java
@@ -0,0 +1,52 @@
+package co.codemaestro.punchclock_beta_v003;
+
+
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+    private CategoryViewModel catViewModel;
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View view = inflater.inflate(R.layout.fragment_home, container, false);
+        final RecyclerView recyclerView = view.findViewById(R.id.recyclerview);
+        final CategoryAdapter adapter = new CategoryAdapter(getContext());
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 1));
+        recyclerView.setAdapter(adapter);
+
+        catViewModel = ViewModelProviders.of(this).get(CategoryViewModel.class);
+
+        catViewModel.getAllCategories().observe(this, new Observer<List<Category>>() {
+            @Override
+            public void onChanged(@Nullable List<Category> categories) {
+                adapter.setCategories(categories);
+            }
+        });
+
+        return view;
+    }
+
+}
--- app/src/main/java/co/codemaestro/punchclock_beta_v003/MainActivity.java
+++ app/src/main/java/co/codemaestro/punchclock_beta_v003/MainActivity.java
@@ -5,18 +5,25 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.transition.Slide;
+import android.transition.Transition;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
 import android.widget.Toast;
 
 import java.util.List;
@@ -32,11 +39,12 @@ public class MainActivity extends AppCompatActivity implements
     private static final int PREFS_MODE = Context.MODE_PRIVATE;
     private static final String nightModeBooleanKey = "co.codemaestro.punchclock_beta_v003.nightModeKey";
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadFragment(new HomeFragment());
+        catViewModel = ViewModelProviders.of(this).get(CategoryViewModel.class);
 
         // Use sharedprefs to reactivate nightMode
         SharedPreferences prefs = getSharedPreferences(PREFS_FILE, PREFS_MODE);
@@ -48,7 +56,7 @@ public class MainActivity extends AppCompatActivity implements
         }
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -64,44 +72,42 @@ public class MainActivity extends AppCompatActivity implements
                 new BottomNavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        Fragment fragment = null;
+
                         switch (menuItem.getItemId()) {
                             case R.id.bottom_nav_home:
-                                // Do nothing?
+                                fragment = new HomeFragment();
                                 break;
                             case R.id.bottom_nav_favorites:
                                 // TODO: code that reloads the recyclerView with favorites
                                 //Intent favIntent = new Intent(getBaseContext(), FavoritesActivity.class);
                                 //startActivity(favIntent);
-                                Toast.makeText(MainActivity.this, "There is no favorites activity", Toast.LENGTH_SHORT).show();
+//                                Toast.makeText(MainActivity.this, "There is no favorites activity", Toast.LENGTH_SHORT).show();
+                                fragment = new FavoritesFragment();
                                 break;
                             case R.id.bottom_nav_settings:
                                 // Intent to SettingsActivity
-                                Intent actionIntent = new Intent(getBaseContext(), SettingsActivity.class);
-                                startActivity(actionIntent);
+//                                Intent actionIntent = new Intent(getBaseContext(), SettingsActivity.class);
+//                                startActivity(actionIntent);
+                                fragment = new SettingsFragment();
                                 break;
                         }
 
-                        return false;
+                        return loadFragment(fragment);
                     }
                 }
         );
+    }
 
-        RecyclerView recyclerView = findViewById(R.id.recyclerview);
-        final CategoryAdapter adapter = new CategoryAdapter(this);
-        recyclerView.setAdapter(adapter);
-        // TODO: Fix spanCount for other orientations
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
-
-        catViewModel = ViewModelProviders.of(this).get(CategoryViewModel.class);
-
-        catViewModel.getAllCategories().observe(this, new Observer<List<Category>>() {
-            @Override
-            public void onChanged(@Nullable List<Category> categories) {
-                adapter.setCategories(categories);
-            }
-        });
-
-
+    private boolean loadFragment(Fragment fragment) {
+        if(fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.containter, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 
 
