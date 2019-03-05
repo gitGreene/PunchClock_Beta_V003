adbc1a4518e1df036e04b6ff3e88fae03de72f0a
Mitch Greene <m.iangreene@gmail.com>
2019-03-05 02:01:36 -0500

    day 144 of 100DoCC

--- app/src/main/java/co/codemaestro/punchclock_beta_v003/Fragments/GoalEntryFormFragment.java
+++ app/src/main/java/co/codemaestro/punchclock_beta_v003/Fragments/GoalEntryFormFragment.java
@@ -5,12 +5,15 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatDialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import co.codemaestro.punchclock_beta_v003.Database.Goal;
@@ -25,10 +28,10 @@ public class GoalEntryFormFragment extends AppCompatDialogFragment {
     private CategoryViewModel categoryViewModel;
     private static int parentCategoryId;
     private TextView parentCategoryIDTextView;
-    private Button addGoalButton;
+    private ImageButton enterStartDate, enterEndDate;
     private String goalName;
     private long desiredGoalTime, goalCycleValue;
-    private EditText enterGoalName, enterDesiredTime, enterGoalCycleValue;
+    private EditText enterGoalName, enterGoalValue, enterGoalCycleValue;
 
     public static GoalEntryFormFragment newInstance(int categoryId) {
         parentCategoryId = categoryId;
@@ -41,32 +44,36 @@ public class GoalEntryFormFragment extends AppCompatDialogFragment {
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+        Dialog dialog = getDialog();
+        if (dialog != null)
+        {
+            int width = ViewGroup.LayoutParams.MATCH_PARENT;
+            int height = ViewGroup.LayoutParams.WRAP_CONTENT;
+            dialog.getWindow().setLayout(width, height);
+        }
+    }
+
+    @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         LayoutInflater inflater = getActivity().getLayoutInflater();
-
         final View view = inflater.inflate(R.layout.fragment_goal_entry_form,null );
-
-        parentCategoryIDTextView = view.findViewById(R.id.parent_category_Id);
         final AlertDialog.Builder newGoalFormBuilder = new AlertDialog.Builder(getActivity());
         newGoalFormBuilder.setView(view);
 
-        enterGoalName = view.findViewById(R.id.enter_goal_edit_text);
-        enterDesiredTime = view.findViewById(R.id.enter_desired_time_edit_text);
-        enterGoalCycleValue = view.findViewById(R.id.enter_goal_cycle_value_edit_text);
 
-        final String goalName = enterGoalName.getText().toString();
+//        int width = getResources().getDimensionPixelSize(R.dimen.popup_width);
+//        int height = getResources().getDimensionPixelSize(R.dimen.popup_height);
+//        getDialog().getWindow().setLayout(width, height);
 
-        categoryViewModel = ViewModelProviders.of(this).get(CategoryViewModel.class);
+        enterStartDate = view.findViewById(R.id.start_date_button);
+        enterEndDate = view.findViewById(R.id.end_date_button);
+        enterGoalName = view.findViewById(R.id.enter_goal_name_et);
+        enterGoalValue = view.findViewById(R.id.enter_goal_value_et);
+        enterGoalCycleValue = view.findViewById(R.id.enter_goal_cycle_value_et);
 
-        addGoalButton = view.findViewById(R.id.add_goal_button);
-        addGoalButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Goal goal = new Goal(parentCategoryId, goalName, 0L, 0L, "24 Hours");
-                categoryViewModel.insertGoal(goal);
-            }
-        });
-
+        // TODO: Make the spinners exist and work properly
 
 
 
